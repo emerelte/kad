cd40f8474859707e476f95b254faebb79c13b116
emerelte <mactob25@gmail.com>
2021-04-14 17:55:11 +0200

    Visualization improvements

--- kad/visualization/visualization.py
+++ kad/visualization/visualization.py
@@ -2,29 +2,45 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
-X_LABEL = "timestamp"
+X_LABEL: str = "timestamp"
+
+PREDICTIONS_COLUMN: str = "predictions"
+ANOMALIES_COLUMN: str = "is_anomaly"
+GROUND_TRUTH_COLUMN: str = "gt_is_anomaly"
+
 
 def visualize(results_df: pd.DataFrame, metric_name: str):
-
+    results_df[ANOMALIES_COLUMN] = results_df[ANOMALIES_COLUMN].fillna(False)
     results_df.index = results_df.index.set_names([X_LABEL])
-    df_subset = results_df.iloc[np.where(results_df["is_anomaly"])]
 
     fig, ax = plt.subplots()
     fig.set_size_inches(12, 8)
     results_df[metric_name].plot.line(ax=ax)
 
-    if np.any(results_df["predictions"]):
+    columns_labels: list = ["Actual TS"]
+
+    if PREDICTIONS_COLUMN in results_df and np.any(results_df[PREDICTIONS_COLUMN]):
         results_df.reset_index().plot.scatter(
             x=X_LABEL,
             y="predictions",
             ax=ax,
             color="g")
+        columns_labels.append("Predictions")
 
-    if np.any(results_df["is_anomaly"]):
-        df_subset.reset_index().plot.scatter(
+    if ANOMALIES_COLUMN in results_df and np.any(results_df[ANOMALIES_COLUMN]):
+        results_df[results_df[ANOMALIES_COLUMN]].reset_index().plot.scatter(
             x=X_LABEL,
             y=metric_name,
             ax=ax,
             color="r")
+        columns_labels.append("Anomalies")
 
-    plt.legend(["Actual TS", "Predictions", "Anomalies"])
+    if GROUND_TRUTH_COLUMN in results_df and np.any(results_df[GROUND_TRUTH_COLUMN]):
+        results_df[results_df[GROUND_TRUTH_COLUMN]].reset_index().plot.scatter(
+            x=X_LABEL,
+            y="value",
+            ax=ax,
+            color="g")
+        columns_labels.append("GT Anomalies")
+
+    plt.legend(columns_labels)
