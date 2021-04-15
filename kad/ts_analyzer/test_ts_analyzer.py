8e66cb18b3772321a9b21865af393e248ca68752
emerelte <mactob25@gmail.com>
2021-04-15 18:45:42 +0200

    Add dominant frequency calculation

--- kad/ts_analyzer/test_ts_analyzer.py
+++ kad/ts_analyzer/test_ts_analyzer.py
@@ -11,7 +11,7 @@ class TestTimeSeriesAnalyzer(unittest.TestCase):
 
     def setUp(self) -> None:
         base = datetime.datetime.today()
-        self.date_list = [base - datetime.timedelta(days=x) for x in range(NUM_DAYS)]
+        self.date_list = [base + datetime.timedelta(days=x) for x in range(NUM_DAYS)]
 
     def test_is_stationary_should_return_true_given_white_noise(self):
         white_noise_arr: np.ndarray = np.random.normal(0, 1, NUM_DAYS)
@@ -23,9 +23,22 @@ class TestTimeSeriesAnalyzer(unittest.TestCase):
         step_set = [-1, 0, 1]
         origin = np.zeros((1, 1))
 
-        step_shape = (NUM_DAYS-1, 1)
+        step_shape = (NUM_DAYS - 1, 1)
         steps = np.random.choice(a=step_set, size=step_shape)
         random_walk_arr = np.concatenate([origin, steps]).cumsum(0)
         random_walk_df = pd.DataFrame(data=random_walk_arr, index=self.date_list)
 
         self.assertFalse(TsAnalyzer(random_walk_df).is_stationary())
+
+    def test_calculate_dominant_frequency(self):
+        t_start = 0
+        t_stop = 400 * np.pi
+        num_samples = NUM_DAYS
+        sin_T = 2 * np.pi
+        expected_freq = round(num_samples / ((t_stop - t_start) / sin_T))
+        t = np.linspace(t_start, t_stop, num_samples)
+        sin_sig = np.sin(t)
+        sin_df = pd.DataFrame(data=sin_sig, index=self.date_list).asfreq("d")
+
+        self.assertEqual(expected_freq, TsAnalyzer(sin_df).calculate_dominant_frequency())
+
--- kad/ts_analyzer/ts_analyzer.py
+++ kad/ts_analyzer/ts_analyzer.py
@@ -1,15 +1,36 @@
+from datetime import timedelta
+
+import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 from statsmodels.tsa.stattools import adfuller
 from kad.kad_utils import kad_utils
+from scipy.fft import fft, fftfreq
+
+
+class TsAnalyzerException(Exception):
+    def __init__(self, message="TS Analyzer Exception"):
+        self.message = message
+
+    def __str__(self):
+        return f"{self.message}"
 
 
 class TsAnalyzer:
     """
     Ts analyzer provides a set of methods describing characteristics of the data
-    Constructor assumes that the DataFrame has only one column
     """
 
     def __init__(self, ts: pd.DataFrame):
+        """
+        :param ts: pd.Dataframe which has one column
+        """
+        required_num_cols = 1
+        num_cols = len(ts.columns)
+        if num_cols != required_num_cols:
+            raise Exception(
+                f"Improper number of columns passed to TsAnalyzer constr: {num_cols}, whereas it should be {required_num_cols}")
+
         self.data = ts
 
     def is_stationary(self):
@@ -28,3 +49,24 @@ class TsAnalyzer:
 
         return kad_utils.get_statistic_test(adf) < kad_utils.get_critical_values(adf)["1%"] \
                and kad_utils.get_pvalue(adf) < significance
+
+    # todo fix accuracy when not much data
+    def calculate_dominant_frequency(self):
+        N = len(self.data)
+        T = 1
+        divisor = 2
+
+        x_fft = fftfreq(N, T)[:N // divisor]
+        y_fft = fft(self.data.to_numpy().flatten())
+        y_fft = np.abs(y_fft[0:N // divisor])
+
+        fig = plt.figure(figsize=(20, 10))
+        ax = fig.add_subplot(111)
+        plt.plot(x_fft, 2.0 / N * y_fft)
+
+        plt.title("FFT")
+        ax.tick_params(axis="x", labelsize=22)
+        ax.tick_params(axis="y", labelsize=22)
+        plt.show()
+
+        return round(1 / (T * x_fft[np.argsort(-y_fft)[1]]))
