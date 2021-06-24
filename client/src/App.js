c3b82a4e26c873d6c6ad6390951b8b2471370719
emerelte <mactob25@gmail.com>
2021-06-24 17:43:57 +0200

    Fetch raw data from client app

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,35 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 
-const REFRESH_TIME_SEC = 1
+const REFRESH_TIME_SEC = 5
 
 class App extends Component {
     state = {
         message: null,
-        source: null
+        source: null,
+        rawData: null
     };
 
     componentDidMount() {
+        // setInterval(() => {
+        //     this.updateImage("http://localhost:5000/plot_results?timestamp=" + new Date().getTime());
+        // }, REFRESH_TIME_SEC * 1000);
         setInterval(() => {
-            this.updateImage("http://localhost:5000/plot_results?timestamp=" + new Date().getTime());
+            this.updateData("http://localhost:5000/get_results?timestamp=" + new Date().getTime());
         }, REFRESH_TIME_SEC * 1000);
+    }
+
+    updateData(url) {
+        axios.get(
+            url,
+            {responseType: "json"}
+        ).then(response => {
+            this.setState({rawData: response.data});
+            this.setState({message: "Data updated"});
+        }).catch(() => {
+            this.setState({message: "Error fetching data!"})
+            this.setState({source: null});
+        });
     }
 
     updateImage(url) {
@@ -36,7 +53,8 @@ class App extends Component {
     }
 
     render() {
-        return this.state.message === null ? <img src={this.state.source}/> : <h>{this.state.message}</h>;
+        // return this.state.message === null ? <img src={this.state.source}/> : <h>{this.state.message}</h>;
+        return <h>{this.state.message}</h>;
     }
 }
 
